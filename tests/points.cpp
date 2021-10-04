1eea03a5ff0a5ec982209adf244080f4188a5e25
Patrick <dreibein@gmail.com>
2021-10-03 20:21:39 -0500

    Added collinear point unit tests

--- tests/points.cpp
+++ tests/points.cpp
@@ -1,4 +1,5 @@
 #include "pdmath/Point.hpp"
+#include "pdmath/Vector.hpp"
 
 #include "catch2/catch_test_macros.hpp"
 #include "catch2/catch_approx.hpp"
@@ -7,6 +8,24 @@ using namespace pdm;
 using namespace Catch;
 
 TEST_CASE("Collinear points test as such", "[points]") {
-    REQUIRE(true);
+    point a(-5, -6, 1);
+    point b(6, -1, 1);
+    point c(-38, -21, 1);
+    REQUIRE(are_collinear(a, b, c));
+
+    a = point(-1, -1, -5);
+    b = point(7, -3, -9);
+    c = point(-3, 8, -3);
+    REQUIRE(!are_collinear(a, b, c));
+
+    a = point(-9, 7, 7);
+    b = point(5, 4, -3);
+    c = point(-37, 13, 27);
+    REQUIRE(are_collinear(a, b, c));
+
+    a = point(-1, 0, -9);
+    b = point(-4, -10, -4);
+    c = point(-3, 2, -4);
+    REQUIRE(!are_collinear(a, b, c));
 }
 
