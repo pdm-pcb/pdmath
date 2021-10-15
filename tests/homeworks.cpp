a2acb9ed7735c9f1d0ec9cf5c7f4249785c7e2fd
Patrick <dreibein@gmail.com>
2021-10-14 21:48:51 -0500

    Rodrigues!

--- tests/homeworks.cpp
+++ tests/homeworks.cpp
@@ -18,25 +18,18 @@ TEST_CASE("Spaceship 'forward' vector manipulation", "[vectors][matrices]") {
     // because j-hat cros k-hat is i-hat
     Vec3 x_vec = y_vec.cross(z_vec);
 
-    Mat3 position(x_vec, y_vec, z_vec);
-
-    std::cout << "Pos:\n" << position << std::endl;
-
     // now we need to rotate left about the 'up' axis by pi/5 radians
     float theta     = std::numbers::pi_v<float> / 5.0f;
     float cos_theta = std::cos(theta);
     float sin_theta = std::sin(theta);
-    Mat3 rot_z(cos_theta,  0,  sin_theta,
-               0,          1,  0,
-               -sin_theta, 0,  cos_theta);
 
-    std::cout << "Rot:\n" << rot_z << std::endl;
+    Vec3 k = y_vec;
+    k.normalize();
+    Vec3 k_cross_v = k.cross(z_vec);
+    float k_dot_v  = k.dot(z_vec);
 
-    Mat3 result = rot_z * position;
+    Vec3 zrot = z_vec * cos_theta + k_cross_v * sin_theta +
+                k * k_dot_v * (1 - cos_theta);
 
-    std::cout << "New:\n" << result << std::endl;
-
-    Vec3 new_z = rot_z * z_vec;
-
-    std::cout << "ZVec:\n" << new_z << std::endl;
+    REQUIRE(zrot == Vec3(-2.5723727, -3.2360680, 0.9543387));
 }
